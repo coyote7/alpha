0602a07d02df60c58ae3e00f7e91651a8d913f7c
coyote7 <odeny7@gmail.com>
2022-07-16 10:39:40 +0300

    discussion forum app

---
+++ base/Supermarket_game.py
@@ -0,0 +1,15 @@
+# welcome user
+name = input("Enter your name")
+items = {'Lottery': 200, 'Bread': 95, 'Milk': 70, 'soda': 60}
+cash = 400
+spent_cash = 0
+q_lottery, q_bread, q_milk, q_soda = 0, 0, 0, 0
+
+
+def welcome():
+    print(f"Welcome {{name}}")
+
+
+
+
+
